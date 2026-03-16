1014bcdaba8b3e3c4aed54fc990d8caccae38a04
Aleksandr Kruglyakov <aleksandr.k.kruglyakov@yandex.ru>
2026-03-17 02:48:38 +0300

    feat: add proxy for no cors request

--- src/shared/api/baseApi.ts
+++ src/shared/api/baseApi.ts
@@ -3,7 +3,7 @@ import { createApi, fetchBaseQuery } from "@reduxjs/toolkit/query/react";
 export const baseApi = createApi({
   reducerPath: "api",
   baseQuery: fetchBaseQuery({
-    baseUrl: "https://dummyjson.com/",
+    baseUrl: "/api/",
     prepareHeaders: (headers) => {
       return headers;
     },
--- vite.config.ts
+++ vite.config.ts
@@ -20,4 +20,13 @@ export default defineConfig({
       "@": path.resolve(__dirname, "./src"),
     },
   },
+  server: {
+    proxy: {
+      "/api": {
+        target: "https://dummyjson.com",
+        changeOrigin: true,
+        rewrite: (path) => path.replace(/^\/api/, ""),
+      },
+    },
+  },
 });
